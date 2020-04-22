1d5abefe934078bf55b35426607be6c8611be1d9
Ryan Morrison <ryanam26@yahoo.com>
2020-04-22 09:07:39 -0400

    changed title and name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Yelp Clone</h1>
+        <h1>Yelp API</h1>
         <SearchBar />
         <BusinessList /> 
       </div>
